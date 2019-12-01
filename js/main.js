76c921150401447f7a74641c7882beeca8a05b90
Sergii Storcheus <sens78@gmail.com>
2019-12-01 21:52:47 +0200

    add section blog

--- js/main.js
+++ js/main.js
@@ -1,33 +1,24 @@
+
 $(document).ready(function() {
-    $('.button[filter = "wd"]').click(function(){
-        if($(this).attr('val') =='off') {
-            $('.button[filter]').attr('val', 'off');
+
+    $('.works-btn[filter]').click(function(){
+        if($(this).attr('filter') =='all') {
+            if($(this).attr('val') =='off') {
+                $('.works-btn[filter]').attr('val', 'off');
+                $(this).attr('val', 'on');
+                $('.works-btn[filter]').removeClass('.focused');
+                $(this).addClass('.focused');
+                $('.filter > div').show(300);
+            }
+        } else
+        if($(this).attr('val')=='off') {
+            $('.works-btn[filter]').attr('val', 'off');
             $(this).attr('val', 'on');
+            $('.works-btn[filter]').removeClass('.focused');
+            $(this).addClass('.focused');
             $('.filter > div').hide(300);
-            $('.filter > div[filter = "wd"]').show(300);
+            var filter = $(this).attr('filter');
+            $('.filter > div[filter='+filter+']').show(300);
         }
-    })
-    $('.button[filter = "moc"]').click(function(){
-        if($(this).attr('val') =='off') {
-            $('.button[filter]').attr('val', 'off');
-            $(this).attr('val', 'on');
-            $('.filter > div').hide(300);
-            $('.filter > div[filter = "moc"]').show(300);
-        }
-    })
-    $('.button[filter = "ud"]').click(function(){
-        if($(this).attr('val') =='off') {
-            $('.button[filter]').attr('val', 'off');
-            $(this).attr('val', 'on');
-            $('.filter > div').hide(300);
-            $('.filter > div[filter = "ud"]').show(300);
-        }
-    })
-    $('.button[filter = "all"]').click(function(){
-        if($(this).attr('val') =='off') {
-            $('.button[filter]').attr('val', 'off');
-            $(this).attr('val', 'on');
-            $('.filter > div').show(300);
-        }
-    })
+    });
 });
